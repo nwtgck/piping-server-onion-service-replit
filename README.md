88002ab296be19920a7ebade8afbd09728ea35d3
Ryo Ota <10933561+nwtgck@users.noreply.github.com>
2021-10-19 10:44:14 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# piping-server-onion-service
+
+[Piping Server](https://github.com/nwtgck/piping-server) as Onion service
+
+## Get hostname
+
+```bash
+cat ./var/lib/tor/hidden_service/hostname
+```
---
+++ start.bash
@@ -0,0 +1,31 @@
+#! /bin/bash -xeu
+
+# Piping Server version
+VERSION=v1.7.0-2
+# binary directory
+BIN_DIR_PATH=$PWD/bin/$VERSION
+BIN_PATH=$BIN_DIR_PATH/piping-server
+
+# Make directory for bin
+mkdir -p $BIN_DIR_PATH
+
+# If bin is not found
+if [ ! -f $BIN_PATH ]; then
+  (
+    # Go to the bin directory
+    cd $BIN_DIR_PATH
+    # Donwload
+    curl -L https://github.com/nwtgck/piping-server-pkg/releases/download/${VERSION}/piping-server-pkg-linuxstatic-x64.tar.gz | tar xzvf -
+    # Move
+    mv piping-server-pkg-linuxstatic-x64/piping-server $BIN_PATH
+    rm -r piping-server-pkg-linuxstatic-x64
+  )
+fi
+
+mkdir -p /home/runner/piping-server-onion-service/var/log/tor/
+mkdir -p /home/runner/piping-server-onion-service/var/lib/tor/hidden_service/
+chmod 700 /home/runner/piping-server-onion-service/var/lib/tor/hidden_service/
+
+$BIN_PATH --http-port=8080 &
+tor -f torrc &
+wait
